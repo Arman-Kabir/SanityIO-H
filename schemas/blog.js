e30fe4b96b5053b4589f3ed02163b3bba9d8a36e
Arman-Kabir <armankabir8@gmail.com>
2023-04-10 10:29:46 +0600

    doing sanity schema

--- schemas/blog.js
+++ schemas/blog.js
@@ -1,28 +1,71 @@
 export default {
-    name: 'blog',
-    type: 'document',
-      title: 'Blog',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'title'
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'title'
+    },
+     {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {
+          type: 'block'
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative text',
+              description: `Some of your visitors cannot see images, 
+                be they blind, color-blind, low-sighted; 
+                alternative text is of great help for those 
+                people that can rely on it to have a good idea of 
+                what\'s on your page.`,
+              options: {
+                isHighlighted: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: 'BlogImage',
+      name: 'blogimage',
+      type: 'image',
+      options: {
+        hotspot: true // <-- Defaults to false
       },
-      {
-        name: 'content',
-        type: 'string',
-        title: 'Content'
-      },
-      {
-        title: 'Launch Scheduled At',
-        name: 'launchAt',
-        type: 'datetime',
-        options: {
-            dateFormat: 'YYYY-MM-DD',
-            timeFormat: 'HH:mm',
-            timeStep: 15,
-            calendarTodayLabel: 'Today'
-          }
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+        },
+        {
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        }
+      ]
+    },
+    {
+      title: 'Created At',
+      name: 'createdAt',
+      type: 'datetime',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        timeFormat: 'HH:mm',
+        timeStep: 15,
+        calendarTodayLabel: 'Today'
       }
-    ]
-  }
+    }
+  ]
+}
